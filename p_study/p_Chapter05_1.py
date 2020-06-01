0df89b5212da596dc12e77039bb407842cd31b13
jhryu1208 <jhryu1208@gmail.com>
2020-06-02 03:02:47 +0900

    JH_python_study

--- p_study/p_Chapter05_1.py
+++ p_study/p_Chapter05_1.py
@@ -1,10 +1,23 @@
 # Chapter 05_1
 # 일급 함수(일급 객체)
 # 파이썬 함수 특징
+
+# 일급 함수(first class)는 객체 지향 프로그래밍(OOP)중에서 파이썬을 포함한
+# 몇몇 프로그래밍 언어에서 발견할 수 있는 개념이다. 아래는 이를 잘드러내는 파이썬의 철학이다.
+#
+# '모든 것은 객체(object)이다.'
+#
+# 객체는 숫자, 문자열, 튜플, 리스트, 딕셔너리, 그리고 함수를 포함한다.
+# 함수는 그 중에서도 일급 시민(first-class citizen)이다. 이 뜻은 다음과 같다.
+#
 # 1. 런타임 초기화
 # 2. 변수 할당 가능
 # 3. 함수 인수 전달 가능
 # 4. 함수 결과 반환 가능(return)
+#
+# 보통은 변수를 객체라고 생각한다. 하지만 함수(function)도, 클래스(class)등도 객체가
+# 될 수 있다. 파이썬의 다른 기타 자료구조를 포함한 모든것의 객체이기 때문에 무엇이든
+# 함수의 인자(argument)로 전달 가능하다.
 
 def factorial(n):
     ''' Factorial Function -> n : int '''
@@ -34,4 +47,63 @@ print(var_func)
 print(var_func(10))
 print(list(map(var_func, range(1, 11))))
 
-# [함수]
+# [함수 인수 전달 및 함수로 결과 반환 -> 고위 함수(Higher-order function)]
+# map, filter, reduce
+print(filter(lambda x: x % 2, range(1,6))) # <filter object at 0x000001F5AF609F88>
+print(map(var_func, filter(lambda x : x % 2, range(1,6)))) # <map object at 0x000001F5AF609FC8>
+print(list(map(var_func, filter(lambda x: x % 2, range(1,6))))) # [1, 6, 120]
+print([var_func(i) for i in range(1, 6) if i % 2]) # [1, 6, 120]
+
+print()
+print()
+
+# [reduce]
+# usage:
+# from functools import reduce
+# reduce(함수, 반복가능한객체)
+
+from functools import reduce
+from operator import add, sub
+
+print(reduce(add, range(1,11))) # 누적계산 : 55
+print(reduce(sub, range(1,11))) # 누적계산 : -53
+print(sum(range(1,11))) # 누적계산 (더간단)
+
+
+# [익명함수(lambda)]
+# 가급적 주석 작성
+# 가급적 익명함수보다는 일반적인 함수 권장
+# 일반 함수 형태로 리팩토링 권장
+
+print(reduce(lambda x, t : x + t, range(1,11)))
+
+print()
+print()
+
+# [callable : 호출 연산자 -> 메소드 형태로 호출 가능한지 확인]
+
+# 호출 가능 확인
+print(callable(str), callable(list), callable(var_func), callable(factorial), callable(3.14))
+# (결과값)
+# True True True True False
+# 3.14는 호출가능한 '함수'가 아니므로, False.
+
+# [partial 사용법 : 인수 고정 -> 콜백 함수 사용]
+from operator import mul
+from functools import partial
+
+# mul()함수는 2개의 인자를 받아서 반환한다.
+print(mul(10,10))
+
+# 인수 고정
+five = partial(mul, 5) # 함수를 인수로 전달 가능, five(a) = 5*a
+                       # 따라서, mul 함수의 인자중 하나가 5로 고정되었다.
+# 고정 추가
+six = partial(five, 6) # six() = 5*6
+                       # 따라서, mul 함수의 나머지 인자가 6으로 고정되었다.
+
+print(five(10)) # 50
+print(six()) # 30
+# print(six(10)) -> TypeError: mul expected 2 arguments, got 3, 인자가 총 3개 들어와서 overflow되어 오류가 발생했다.
+print([five(i) for i in range(1,11)]) # 5의 배수 형성 : [5, 10, 15, 20, 25, 30, 35, 40, 45, 50]
+print(list(map(five, range(1,11)))) # 좀더 복잡한 5의 배수 형성
---
+++ p_study/p_Chapter05_2.py
@@ -0,0 +1,108 @@
+# Chapter 05_2
+# 일급 함수(일급 객체)
+# 클로저 기초
+
+# 파이썬 변수 범위(scope)
+
+# [ex1]
+def func_v1(a):
+    print(a)
+    print(b)
+
+# func_v1(10) -> 인자 b가 없으므로 에러
+
+# [ex2]
+b = 20
+
+def func_v2(a):
+    print(a)
+    print(b)
+
+func_v2(10)
+
+# ex2를 통해 알 수 있는 것은 파이썬에서 선언된 함수는 지역변수말고도,
+# 파이썬 내부 인터프리터를 통해서,
+# 글로벌 변수 또한 함수내에서 받아들인다는 것을 알 수 있다.
+
+# [ex3-1]
+c = 30
+
+def func_v3(a):
+    c = 40      # print()위에 지역변수 c를 선언할 경우 정상 출력되는 것을 확인 할 수 있다.
+    print(a)
+    print(c)
+#   c = 40 -> c변수가 하단에서 선언된 경우,
+#             UnboundLocalError: local variable 'c' referenced before assignment
+#             의 에러 문구에서 global 변수보다 local변수가 먼저 참조되는 것을 알 수있다.
+
+print('global ', c) # 이때는 glbal 변수 c = 30이 출력됨을 확인 할 수 있다.
+func_v3(10) # 10 40
+
+# 하지만, 함수내에서 지역변수가 선언되어있어도, 글로벌 변수를 사용 할 수 없는 것은 아니다.
+# 다음 예제를 참조하자.
+
+# [ex3-2]
+c = 30
+
+def func_v4(a):
+    global c # global 변수 c 호출
+    print(a)
+    print(c)
+    c = 40  # local 변수를 하단에 선언
+
+print('global ', c) # 30
+func_v4(10) # 10 30 -> local 변수가 선언되어 있음에도 불구하고, global 변수 c가 쓰였음을 확인 할 수있다.
+
+# 하지만, global 변수 c를 다시 출력해보면 40이 출력됨을 확인 할 수있다.
+# 글로벌 변수 c의 값이 지역변수 c의값으로 바뀌었다.
+print('local -> global ', c)
+
+# Closure(클로저) 사용 이유
+# 서버 프로그래밍 -> 동시성(Concurrency)제어 -> 메모리 공간에 여러 자원이 접근 -> 교착 상태(Dead Lock)
+# 메모리를 공유하지 않고 메시지 전달로 처리하기 위한 -> Erlang
+# 클로저는 공유하되 변경하지 않는(Immutable, Read Only) 적극적으로 사용 -> 함수형 프로그래밍
+# 클로저는 불변자료구조 및 atom, STM -> 멀티스레드(Coroutine) 프로그램에 강점
+# 더 간단하게, 클로저는 불변인 상태를 기억한다.
+
+a = 100
+
+print(a + 100)  # 200
+print(a + 1000) # 1100
+
+# 결과 누적(함수 사용)
+print(sum(range(1,51)))
+print(sum(range(51,101)))
+
+# 클래스 이용
+class Averager():
+    def __init__(self):
+        self._series = []
+
+    def __call__(self, v):
+        self._series.append(v)
+        print('inner >> {} / {}'.format(self._series, len(self._series)))
+        return sum(self._series) / len(self._series)
+
+# 인스턴스 생성
+averager_cls = Averager()
+
+# 누적
+
+print(averager_cls(10)) # 클래스를 함수처럼 사용하는 것에 한번 주목
+print(averager_cls(30))
+print(averager_cls(50))
+print(averager_cls(70))
+print(averager_cls(90))
+
+# inner >> [10] / 1
+# 10.0
+# inner >> [10, 30] / 2
+# 20.0
+# inner >> [10, 30, 50] / 3
+# 30.0
+# inner >> [10, 30, 50, 70] / 4
+# 40.0
+# inner >> [10, 30, 50, 70, 90] / 5
+# 50.0
+
+# 상태를 누적하고 있음을 확인 할 수 있다.
